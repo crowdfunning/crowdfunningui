51fc4c9b092a7f9fec72b9dfe71adf93cde22139
crowdfunning <55283382+crowdfunning@users.noreply.github.com>
2019-09-16 01:27:37 +0530

    hash location implemented

--- crowdfunning/src/app/app-routing.module.ts
+++ crowdfunning/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   { path: 'homepage',  component  : HomepageComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
